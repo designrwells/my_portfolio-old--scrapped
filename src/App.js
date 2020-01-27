438fd0b3a0f8c6963d38270f051d1535f3a0741b
Darrin Wells <dwells@ok7-eleven.com>
2020-01-27 16:44:30 -0600

    started routing pages and setting up bare bones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './Projects.js';
+import Articles from './Articles.js';
+import About from './About.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
+
+        <div className="navbar">
+          <div className="navbar-sub">
+
+            <Link to="/" className="nav-item">Projects</Link>
+            <Link to="/articles" className="nav-item">Articles</Link>
+            <Link to="about" className="nav-item">About</Link>
+          
+          </div>
+        </div>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
