1db9e53d6551a325382b31f2e3846e8bf0878b31
Darrin Wells <designrwells@gmail.com>
2020-02-21 21:08:28 -0600

    Added images and made more progress on About page functionality/animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react'
 import { CSSTransition, Transition, TransitionGroup } from 'react-transition-group';
 import { TimelineLite, CSSPlugin } from 'gsap';
- 
+import octopusRight from '../images/octopus-right.png';
+import octopusLeft from '../images/octopus-left.png';
+
 class About extends Component {
 
 	constructor(props){
 		super(props);
-
-		this.aboutContainer = null;
 
 		this.aboutTween = null;
 	}
 
 	componentDidMount() {
 		this.aboutTween = new TimelineLite({ paused:true })
-			.to(this.aboutContainerLeft, 1.4, { opacity: 0, x: -350 })
-			.to(this.aboutContainerRight, 1.4, { opacity: 0, x: 350 }, '-=1.4');
+			.to(this.aboutContainerLeft, 1.3, { opacity: 0, x: -350 })
+			.to(this.aboutContainerRight, 1.3, { opacity: 0, x: 350 }, '-=1.3')
+			.to(this.aboutHeader, 1.5, { opacity: 1, y: 150 }, '-=.3' );
 	}
 
 	render() {
 		  return (
 		    <div className="body-wrapper">
 		    	<div className="about-wrapper" onClick={ () => this.aboutTween.play() }>
-		    		<div className="about-block-left" ref={ div  => this.aboutContainerLeft = div }>Block Left</div>
-		    		<h1 className="about-header">ABOUT</h1>
-		    		<div className="about-block-right" ref={ div  => this.aboutContainerRight = div }>Block Right</div>
+		    		<img src={octopusLeft} className="about-block-left" ref={ img  => this.aboutContainerLeft = img } />
+		    		<h1 className="about-header" ref={ h1 => this.aboutHeader = h1 }>ABOUT</h1>
+		    		<img src={octopusRight} className="about-block-right" ref={ img  => this.aboutContainerRight = img } />
 			    </div>
 		    </div>
 		  )
