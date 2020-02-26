30c555aa6018d3841a66e4ecfe95fa7881cea056
Darrin <designrwells@gmail.com>
2020-02-26 13:25:46 -0600

    removed Home page attributes and added flowing water to all pages. Some stellar ideas on the way!

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,62 +9,7 @@ function Home() {
 
 	return (
 		<div className="body-wrapper">
-			<div id="carousel-wrapper" className="page-content-wrapper">
-
-				<Carousel 
-					pauseOnHover={true}
-					touch={true}
-					controls={false}
-					wrap={true}
-					>
-
-				  <Carousel.Item>
-				    <div className="pulsate-bck">
-			      		<div className="anim-text">Hey, I'm Darrin</div>
-			      	</div>
-				    <Carousel.Caption>
-				    	That is my name.
-				    </Carousel.Caption>
-				  </Carousel.Item>
-
-				  <Carousel.Item>
-				    <div className="pulsate-bck">
-			      		<div className="anim-text">Web wizardry</div>
-			      	</div>
-				    <Carousel.Caption>
-				    	That is my game.
-				    </Carousel.Caption>
-				  </Carousel.Item>
-
-				  <Carousel.Item>
-				    <div className="pulsate-bck">
-			      		<div className="anim-text">Here's some text</div>
-			      	</div>
-				    <Carousel.Caption>
-				    	And here's some more text.
-				    </Carousel.Caption>
-				  </Carousel.Item>
-
-				  <Carousel.Item>
-				    <div className="pulsate-bck">
-			      		<div className="anim-text">Something else here</div>
-			      	</div>
-				    <Carousel.Caption>
-				    	And one more thing here.
-				    </Carousel.Caption>
-				  </Carousel.Item>
-
-				  <Carousel.Item>
-				    <div className="pulsate-bck">
-			      		<div className="anim-text">Final slide</div>
-			      	</div>
-				    <Carousel.Caption>
-				    	Slide on man, slide on.
-				    </Carousel.Caption>
-				  </Carousel.Item>
-
-				</Carousel>
-			</div>
+			
 		</div>
 			  )
 			}
